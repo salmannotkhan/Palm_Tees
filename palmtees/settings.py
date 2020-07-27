4254501dd2aaf7449bb41d8d3b34eafe58d81df5
Salman Shaikh <tony903212@gmail.com>
2020-07-28 00:44:28 +0530

    Fixed settings.py

--- palmtees/settings.py
+++ palmtees/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = 'product/'
