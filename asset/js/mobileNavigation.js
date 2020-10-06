0df2fffce750fa0d4e1d02f1e921ec1ac81c1259
Salman Shaikh <tony903212@gmail.com>
2020-10-06 16:28:54 +0530

    Code Optimization and Fixes Disable magnifier in mobile devices Fixed Notification z-index Separated Stylesheets for every page(to load page faster) Code Cleanup

--- asset/js/mobileNavigation.js
+++ asset/js/mobileNavigation.js
@@ -2,12 +2,12 @@ const navBtn = document.querySelector('.navbtn')
 const navbar = document.querySelector('nav')
 navBtn.addEventListener('click', ()=>{
     margin = window.getComputedStyle(navbar).marginTop
-    if (margin === '-235px') {
+    if (margin === '-240px') {
         navbar.style.marginTop = '0%'
         navBtn.classList.add('close')
     }
     else {
-        navbar.style.marginTop = '-235px'
+        navbar.style.marginTop = '-240px'
         navBtn.classList.remove('close')
     }
 })
