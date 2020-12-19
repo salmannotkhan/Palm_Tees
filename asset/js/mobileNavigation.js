a8cf508cd63b4ce605b5b36c3ef4bceab3d4dd43
Salman Shaikh <tony903212@gmail.com>
2020-12-20 00:48:57 +0530

    Introducing Dark Mode

--- asset/js/mobileNavigation.js
+++ asset/js/mobileNavigation.js
@@ -1,5 +1,7 @@
 const navBtn = document.querySelector('.navbtn')
 const navbar = document.querySelector('nav')
+const darkMode = document.querySelector('.switch')
+const body = document.querySelector('body')
 navBtn.addEventListener('click', ()=>{
     margin = window.getComputedStyle(navbar).marginTop
     if (margin === '-240px') {
@@ -11,3 +13,16 @@ navBtn.addEventListener('click', ()=>{
         navBtn.classList.remove('close')
     }
 })
+function nightMode(){
+    status = document.querySelector('body').classList.toggle('dark')
+    darkMode.classList.toggle('activated')
+    darkMode.children[0].children[0].classList.toggle('hide')
+    darkMode.children[0].children[1].classList.toggle('hide')
+    return status
+}
+darkMode.addEventListener('click', () => {
+    localStorage.setItem('darkmode', nightMode())
+})
+if (localStorage.darkmode == 'true'){
+    nightMode()
+}
